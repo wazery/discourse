18e14dc21c6f7e2ace69582aa3937e41cf9a4fd1
Neil Lalonde <neillalonde@gmail.com>
2014-04-22 17:47:16 -0400

    Some client-side validation of color fields

--- app/assets/javascripts/admin/components/color_input_component.js
+++ app/assets/javascripts/admin/components/color_input_component.js
@@ -12,19 +12,11 @@ Discourse.ColorInputComponent = Ember.Component.extend({
   layoutName: 'components/color-input',
 
   hexValueChanged: function() {
-    if (this.get('hexValue')) {
-      this.set('hexValue', this.get('hexValue').replace(/[^0-9a-fA-F]/g, ''));
-      var hex = this.get('hexValue');
-      if (hex.length === 6 || hex.length === 3) {
-        if (hex.length === 3) {
-          hex = hex.substr(0,1) + hex.substr(0,1) + hex.substr(1,1) + hex.substr(1,1) + hex.substr(2,1) + hex.substr(2,1);
-        }
-        var brightness = Math.round(((parseInt('0x'+hex.substr(0,2)) * 299) + (parseInt('0x'+hex.substr(2,2)) * 587) + (parseInt('0x'+hex.substr(4,2)) * 114)) /1000),
-            style = 'color: ' + (brightness > 125 ? 'black' : 'white') + '; background-color: #' + this.get('hexValue') + ';';
-        this.$('input').attr('style', style);
-      }
+    var hex = this.get('hexValue');
+    if (hex && (hex.length === 3 || hex.length === 6) && this.get('brightnessValue')) {
+      this.$('input').attr('style', 'color: ' + (this.get('brightnessValue') > 125 ? 'black' : 'white') + '; background-color: #' + hex + ';');
     }
-  }.observes('hexValue'),
+  }.observes('hexValue', 'brightnessValue'),
 
   didInsertElement: function() {
     var self = this;
--- app/assets/javascripts/admin/models/color_scheme_color.js
+++ app/assets/javascripts/admin/models/color_scheme_color.js
@@ -8,6 +8,7 @@
   @module Discourse
 **/
 Discourse.ColorSchemeColor = Discourse.Model.extend({
+
   init: function() {
     this._super();
     this.startTrackingChanges();
@@ -36,5 +37,35 @@ Discourse.ColorSchemeColor = Discourse.Model.extend({
       this.set('hex',     this.originals['hex']);
       this.set('opacity', this.originals['opacity']);
     }
-  }
+  },
+
+  /**
+    brightness returns a number between 0 (darkest) to 255 (brightest).
+    Undefined if hex is not a valid color.
+
+    @property brightness
+  **/
+  brightness: function() {
+    var hex = this.get('hex');
+    if (hex.length === 6 || hex.length === 3) {
+      if (hex.length === 3) {
+        hex = hex.substr(0,1) + hex.substr(0,1) + hex.substr(1,1) + hex.substr(1,1) + hex.substr(2,1) + hex.substr(2,1);
+      }
+      return Math.round(((parseInt('0x'+hex.substr(0,2)) * 299) + (parseInt('0x'+hex.substr(2,2)) * 587) + (parseInt('0x'+hex.substr(4,2)) * 114)) /1000);
+    }
+  }.property('hex'),
+
+  hexValueChanged: function() {
+    if (this.get('hex')) {
+      this.set('hex', this.get('hex').toString().replace(/[^0-9a-fA-F]/g, ""));
+    }
+  }.observes('hex'),
+
+  opacityChanged: function() {
+    if (this.get('opacity')) {
+      var o = this.get('opacity').toString().replace(/[^\d.]/g, "");
+      if (parseInt(o,10) > 100) { o = o.substr(0,o.length-1); }
+      this.set('opacity', o);
+    }
+  }.observes('opacity')
 });
